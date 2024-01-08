1ce46183f0a2beb7b17e4a77c8d219e021bbd076
Oscar Buon Ryd <oscardoras@gmail.com>
2024-01-08 23:56:47 +0100

    Update.

--- src/interpreter/system_functions/Dll.cpp
+++ src/interpreter/system_functions/Dll.cpp
@@ -86,6 +86,108 @@ namespace Interpreter::SystemFunctions {
         }
 
 
+        void compile(std::set<std::filesystem::path> const& include_path, std::set<std::filesystem::path> const& files, std::filesystem::path const& out) {
+            std::string cmd = "g++ -g -Wall -Wextra -shared";
+            if (!include_path.empty()) {
+                cmd += " -I";
+                for (auto const& p : include_path)
+                    cmd += " " + p.string();
+            }
+            cmd += " -o " + out.string();
+            for (auto const& p : files)
+                cmd += " " + p.string();
+
+            system(cmd.c_str());
+        }
+
+        struct CSymbol {
+            std::string symbol;
+            std::string include;
+        };
+
+        auto getter_args = std::make_shared<Parser::Symbol>("var");
+        Reference getter_c(FunctionContext& context) {
+            auto ref = context["var"];
+            if (auto property_reference = std::get_if<PropertyReference>(&ref)) {
+                try {
+                    auto c_symbol = std::any_cast<CSymbol>(property_reference->parent.get().c_obj);
+
+
+                } catch (std::bad_any_cast const&) {}
+            }
+        }
+
+        auto c_function_args = std::make_shared<Parser::FunctionCall>(
+            std::make_shared<Parser::Symbol>("function"),
+            std::make_shared<Parser::Tuple>()
+        );
+        Reference c_function(FunctionContext& context) {
+            auto self = context["this"].to_data(context).get<Object*>();
+            auto c_symbol = std::any_cast<CSymbol>(self->c_obj);
+            auto args = std::get<CustomFunction>(context["function"].to_data(context).get<Object*>()->functions.front())->body;
+
+            if (auto tuple = std::dynamic_pointer_cast<Parser::Tuple>(args)) {
+                auto& parent = context.get_parent();
+
+                std::vector<Data> data;
+                for (auto arg : tuple->objects)
+                    data.push_back(Interpreter::execute(parent, arg).to_data(context));
+
+                {
+                    std::filesystem::create_directory("/tmp/ouverium_dll");
+                    auto file = std::ofstream("/tmp/ouverium_dll/call.cpp");
+                    file << "#include \"" << c_symbol.include << "\"" << std::endl;
+                    file << "void* ptr = (void*) &" << symbol << ";" << std::endl;
+                    file << std::endl;
+                    file.close();
+                }
+            }
+        }
+
+        Reference import_h(FunctionContext& context) {
+            auto path = get_canonical_path(context);
+
+            if (std::set<std::string>{".h", ".hpp"}.contains(path.extension())) {
+                auto& global = context.get_global();
+                auto root = context.expression->get_root();
+
+                auto it = global.sources.find(path);
+                if (it == global.sources.end()) {
+                    global.sources[path] = std::make_shared<Parser::Tuple>();
+
+                    auto symbols = root->symbols;
+                    for (auto const& symbol : context.expression->get_root()->get_symbols()) {
+                        bool c_symbol = true;
+                        for (char c : symbol) {
+                            if (!(std::isalnum(c) || c == '_')) {
+                                c_symbol = false;
+                                break;
+                            }
+                        }
+                        if (!c_symbol)
+                            continue;
+
+                        std::filesystem::create_directory("/tmp/ouverium_dll");
+                        auto file = std::ofstream("/tmp/ouverium_dll/header.cpp");
+                        file << "#include " << path << std::endl;
+                        file << "void* ptr = (void*) &" << symbol << ";" << std::endl;
+                        file << std::endl;
+                        file.close();
+
+                        if (system("g++ -shared -o /tmp/ouverium_dll/libheader.so /tmp/ouverium_dll/header.cpp 2> /dev/null") == 0) {
+                            symbols.insert(symbol);
+
+                            global[symbol].to_data(context);
+                        }
+                    }
+                    root->compute_symbols(symbols);
+                }
+
+                return Reference();
+            } else throw Interpreter::FunctionArgumentsError();
+        }
+
+
         /*
                 struct DllSymbol {
                     void* ptr;
@@ -148,8 +250,10 @@ namespace Interpreter::SystemFunctions {
                 }
         */
 
+
         void init(GlobalContext& context) {
             context.get_function("import").push_front(SystemFunction{ path_args, import });
+            context.get_function("import").push_front(SystemFunction{ path_args, import_h });
         }
 
     }
