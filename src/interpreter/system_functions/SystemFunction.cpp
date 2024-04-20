711b85478f9b81d169818e47d36862830dceb250
Oscar Buon Ryd <oscardoras@gmail.com>
2024-04-20 15:56:43 +0200

    overloaded helper

--- src/interpreter/system_functions/SystemFunction.cpp
+++ src/interpreter/system_functions/SystemFunction.cpp
@@ -3,6 +3,8 @@
 
 template<class... Ts>
 struct overloaded : Ts... { using Ts::operator()...; };
+template<class... Ts>
+overloaded(Ts...) -> overloaded<Ts...>;
 
 namespace Interpreter::SystemFunctions {
 
