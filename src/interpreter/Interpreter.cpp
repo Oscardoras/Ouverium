b654c59fa8c9ff14bfa2c89ce00be852266e61a5
Oscar Buon Ryd <oscardoras@gmail.com>
2025-06-01 18:38:59 +0200

    added setter in parameters

--- src/interpreter/Interpreter.cpp
+++ src/interpreter/Interpreter.cpp
@@ -3,6 +3,8 @@
 #include <list>
 #include <map>
 #include <memory>
+#include <optional>
+#include <set>
 #include <sstream>
 #include <string>
 #include <utility>
@@ -70,6 +72,9 @@ namespace Interpreter {
 
         Reference compute(Context& context, Arguments const& arguments) {
             if (auto const* expression = std::get_if<ParserExpression>(&arguments)) {
+                if (*expression == nullptr)
+                    throw Interpreter::FunctionArgumentsError();
+
                 auto r = Interpreter::execute(context, *expression);
                 operator[](*expression) = r;
                 return r;
@@ -83,24 +88,71 @@ namespace Interpreter {
 
     };
 
-    void set_arguments(Context& context, FunctionContext& function_context, Computed& computed, std::shared_ptr<Parser::Expression> const& parameters, Arguments const& argument) {
+    bool check_tuple(Context& context, FunctionContext& function_context, std::shared_ptr<Parser::Tuple> const& parameters, size_t args) {
+        if (parameters->objects.size() < args) return false;
+
+        for (size_t i = args; i < parameters->objects.size(); ++i) {
+            auto p_function = std::dynamic_pointer_cast<Parser::FunctionCall>(parameters->objects[i]);
+            if (!p_function) return false;
+
+            auto r = execute(function_context, p_function->function).to_data(context, p_function);
+            if (r != context["setter"].get_data()) return false;
+
+            auto tuple = std::dynamic_pointer_cast<Parser::Tuple>(p_function->arguments);
+            if (!tuple || tuple->objects.size() != 2) return false;
+        }
+
+        return true;
+    }
+
+    std::optional<std::pair<std::string, Arguments>> is_explicit(Context& context, std::shared_ptr<Parser::Expression> const& parameters, Arguments const& argument) {
+        auto const* e = std::get_if<ParserExpression>(&argument);
+        if (!e) return std::nullopt;
+
+        auto a_function = std::dynamic_pointer_cast<Parser::FunctionCall>(*e);
+        if (!a_function) return std::nullopt;
+
+        auto a_symbol = std::dynamic_pointer_cast<Parser::Symbol>(a_function->function);
+        if (!a_symbol) return std::nullopt;
+
+        auto r = execute(context, a_symbol).to_data(context, parameters);
+        if (r != context["setter"].get_data()) return std::nullopt;
+
+        auto tuple = std::dynamic_pointer_cast<Parser::Tuple>(a_function->arguments);
+        if (!tuple || tuple->objects.size() != 2) return std::nullopt;
+
+        auto a_property = std::dynamic_pointer_cast<Parser::Property>(tuple->objects[0]);
+        if (!a_property) return std::nullopt;
+
+        if (!std::get_if<std::weak_ptr<Parser::Expression>>(&a_property->object)) return std::nullopt;
+
+        return std::pair<std::string, Arguments>{ a_property->name, tuple->objects[1] };
+    }
+
+    void set_arguments(Context& context, FunctionContext& function_context, std::set<std::string> const& symbols, Computed& computed, std::shared_ptr<Parser::Expression> const& parameters, Arguments const& argument) {
         auto arguments = computed.get(argument);
 
         if (auto symbol = std::dynamic_pointer_cast<Parser::Symbol>(parameters)) {
-            auto reference = computed.compute(context, arguments);
-
-            if (function_context.has_symbol(symbol->name)) {
-                if (reference != Reference(function_context[symbol->name]))
-                    throw Interpreter::FunctionArgumentsError();
+            if (auto e = is_explicit(context, parameters, arguments); e && symbols.contains(e->first)) {
+                function_context.add_symbol(e->first, computed.compute(context, e->second));
             } else {
-                function_context.add_symbol(symbol->name, reference);
+                auto reference = computed.compute(context, arguments);
+
+                if (function_context.has_symbol(symbol->name)) {
+                    if (reference != Reference(function_context[symbol->name]))
+                        throw Interpreter::FunctionArgumentsError();
+                } else {
+                    function_context.add_symbol(symbol->name, reference);
+                }
             }
         } else if (auto p_tuple = std::dynamic_pointer_cast<Parser::Tuple>(parameters)) {
             if (auto* expression = std::get_if<ParserExpression>(&arguments)) {
                 if (auto a_tuple = std::dynamic_pointer_cast<Parser::Tuple>(*expression)) {
-                    if (p_tuple->objects.size() == a_tuple->objects.size()) {
-                        for (size_t i = 0; i < p_tuple->objects.size(); ++i)
-                            set_arguments(context, function_context, computed, p_tuple->objects[i], a_tuple->objects[i]);
+                    if (check_tuple(context, function_context, p_tuple, a_tuple->objects.size())) {
+                        for (size_t i = 0; i < p_tuple->objects.size(); ++i) {
+                            auto const& arg = i < a_tuple->objects.size() ? a_tuple->objects[i] : nullptr;
+                            set_arguments(context, function_context, symbols, computed, p_tuple->objects[i], arg);
+                        }
 
                         std::vector<Arguments> cache;
                         for (auto const& o : a_tuple->objects) {
@@ -114,23 +166,44 @@ namespace Interpreter {
                     } else
                         throw Interpreter::FunctionArgumentsError();
                 } else {
-                    set_arguments(context, function_context, computed, parameters, computed.compute(context, arguments));
+                    set_arguments(context, function_context, symbols, computed, parameters, computed.compute(context, arguments));
                 }
             } else if (auto* reference = std::get_if<Reference>(&arguments)) {
                 try {
-                    auto object = reference->to_data(function_context, parameters).get<ObjectPtr>();
-                    if (object->array.size() == p_tuple->objects.size()) {
-                        for (size_t i = 0; i < p_tuple->objects.size(); ++i)
-                            set_arguments(context, function_context, computed, p_tuple->objects[i], Data(object).get_at(i));
-                    } else
-                        throw Interpreter::FunctionArgumentsError();
+                    try {
+                        if (p_tuple->objects.size() > 0) {
+                            if (auto p_function = std::dynamic_pointer_cast<Parser::FunctionCall>(p_tuple->objects[0])) {
+                                auto r = execute(function_context, p_function->function).to_data(context, p_function);
+                                if (r == context["setter"].get_data()) {
+                                    if (auto tuple = std::dynamic_pointer_cast<Parser::Tuple>(p_function->arguments); tuple && tuple->objects.size() == 2)
+                                        set_arguments(context, function_context, symbols, computed, p_tuple, std::vector<Arguments>{ *reference });
+                                    else
+                                        throw Interpreter::FunctionArgumentsError();
+                                } else
+                                    throw Interpreter::FunctionArgumentsError();
+                            } else
+                                throw Interpreter::FunctionArgumentsError();
+                        } else
+                            throw Interpreter::FunctionArgumentsError();
+                    } catch (const Interpreter::FunctionArgumentsError&) {
+                        auto object = reference->to_data(function_context, parameters).get<ObjectPtr>();
+                        if (check_tuple(context, function_context, p_tuple, object->array.size())) {
+                            for (size_t i = 0; i < p_tuple->objects.size(); ++i) {
+                                auto const& arg = i < object->array.size() ? Arguments(Data(object).get_at(i)) : ParserExpression(nullptr);
+                                set_arguments(context, function_context, symbols, computed, p_tuple->objects[i], arg);
+                            }
+                        } else
+                            throw Interpreter::FunctionArgumentsError();
+                    }
                 } catch (Data::BadAccess const&) {
                     throw Interpreter::FunctionArgumentsError();
                 }
             } else if (auto* vector = std::get_if<std::vector<Arguments>>(&arguments)) {
-                if (vector->size() == p_tuple->objects.size()) {
-                    for (size_t i = 0; i < p_tuple->objects.size(); ++i)
-                        set_arguments(context, function_context, computed, p_tuple->objects[i], (*vector)[i]);
+                if (check_tuple(context, function_context, p_tuple, vector->size())) {
+                    for (size_t i = 0; i < p_tuple->objects.size(); ++i) {
+                        auto const& arg = i < (*vector).size() ? (*vector)[i] : ParserExpression(nullptr);
+                        set_arguments(context, function_context, symbols, computed, p_tuple->objects[i], arg);
+                    }
                 } else
                     throw Interpreter::FunctionArgumentsError();
             }
@@ -166,19 +239,36 @@ namespace Interpreter {
             } else {
                 auto r = execute(function_context, p_function->function).to_data(context, parameters);
 
-                Arguments args;
-                if (auto* expression = std::get_if<ParserExpression>(&arguments)) {
-                    auto it = computed.find(*expression);
-                    args = it != computed.end() ? it->second : arguments;
+                if (r == context["setter"].get_data()) {
+                    if (auto tuple = std::dynamic_pointer_cast<Parser::Tuple>(p_function->arguments); tuple && tuple->objects.size() == 2) {
+                        if (auto* ptr = std::get_if<std::shared_ptr<Parser::Expression>>(&arguments)) {
+                            if (*ptr == nullptr) {
+                                set_arguments(context, function_context, symbols, computed, tuple->objects[0], tuple->objects[1]);
+                            } else if (auto empty_tuple = std::dynamic_pointer_cast<Parser::Tuple>(*ptr); empty_tuple && empty_tuple->objects.empty()) {
+                                set_arguments(context, function_context, symbols, computed, tuple->objects[0], tuple->objects[1]);
+                            } else {
+                                set_arguments(context, function_context, symbols, computed, tuple->objects[0], arguments);
+                            }
+                        } else {
+                            set_arguments(context, function_context, symbols, computed, tuple->objects[0], arguments);
+                        }
+                    } else
+                        throw Interpreter::FunctionArgumentsError();
                 } else {
-                    args = arguments;
-                }
+                    Arguments args;
+                    if (auto* expression = std::get_if<ParserExpression>(&arguments)) {
+                        auto it = computed.find(*expression);
+                        args = it != computed.end() ? it->second : arguments;
+                    } else {
+                        args = arguments;
+                    }
 
-                auto const result = try_call_function(context, p_function, r, args);
-                if (auto const* reference = std::get_if<Reference>(&result)) {
-                    set_arguments(context, function_context, computed, p_function->arguments, *reference);
-                } else
-                    throw Interpreter::FunctionArgumentsError();
+                    auto const result = try_call_function(context, p_function, r, args);
+                    if (auto const* reference = std::get_if<Reference>(&result)) {
+                        set_arguments(context, function_context, symbols, computed, p_function->arguments, *reference);
+                    } else
+                        throw Interpreter::FunctionArgumentsError();
+                }
             }
         } else if (auto p_property = std::dynamic_pointer_cast<Parser::Property>(parameters)) {
             auto reference = computed.compute(context, arguments);
@@ -186,7 +276,7 @@ namespace Interpreter {
             if (auto* property_reference = std::get_if<PropertyReference>(&reference)) {
                 if (p_property->name == property_reference->name || p_property->name == ".") {
                     if (auto* ptr = std::get_if<std::shared_ptr<Parser::Expression>>(&p_property->object)) {
-                        set_arguments(context, function_context, computed, *ptr, Reference(property_reference->parent));
+                        set_arguments(context, function_context, symbols, computed, *ptr, Reference(property_reference->parent));
                     } else
                         throw Interpreter::FunctionArgumentsError();
                 } else
@@ -221,7 +311,11 @@ namespace Interpreter {
                     function_context.add_symbol(symbol.first, symbol.second);
 
                 if (auto const* custom_function = std::get_if<CustomFunction>(&function)) {
-                    set_arguments(context, function_context, computed, (*custom_function)->parameters, arguments);
+                    std::set<std::string> symbols;
+                    for (auto const& symbol : (*custom_function)->parameters->symbols)
+                        symbols.emplace(symbol);
+
+                    set_arguments(context, function_context, symbols, computed, (*custom_function)->parameters, arguments);
 
                     Data filter = Data(true);
                     if ((*custom_function)->filter != nullptr)
@@ -236,7 +330,7 @@ namespace Interpreter {
                         throw FunctionArgumentsError();
                     }
                 } else if (auto const* system_function = std::get_if<SystemFunction>(&function)) {
-                    set_arguments(context, function_context, computed, system_function->parameters, arguments);
+                    set_arguments(context, function_context, {}, computed, system_function->parameters, arguments);
 
                     return system_function->pointer(function_context);
                 } else
